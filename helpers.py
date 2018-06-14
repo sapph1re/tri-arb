7bc3ef40f47553fd4d2dd6f6cd5a6dbb69a08b73
drag0no <krotpv@gmail.com>
2018-06-14 14:37:17 +0700

    Added "threaded" decorator with Future

--- helpers.py
+++ helpers.py
@@ -1,4 +1,22 @@
 import time
+from threading import Thread
+from concurrent.futures import Future
+
+
+def call_with_future(fn, future, args, kwargs):
+    try:
+        result = fn(*args, **kwargs)
+        future.set_result(result)
+    except Exception as e:
+        future.set_exception(e)
+
+
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        future = Future()
+        Thread(target=call_with_future, args=(fn, future, args, kwargs)).start()
+        return future
+    return wrapper
 
 
 def timing(f):
