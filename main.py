72b2577566eb5ff6d91cde73654fb5168b0bd9ad
Roman V <dev.romanv@gmail.com>
2018-12-11 00:37:37 +0700

    Restructured main properly

--- main.py
+++ main.py
@@ -3,27 +3,35 @@ from config import API_KEY, API_SECRET, TRADE_FEE, MIN_PROFIT
 from binance_api import BinanceApi
 from arbitrage_detector import ArbitrageDetector
 from binance_actions_executor import BinanceActionsExecutor, BinanceSingleAction
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QCoreApplication, QObject
 from custom_logging import get_logger
 logger = get_logger(__name__)
 
 
-if __name__ == '__main__':
-    logger.info('Starting...')
-    app = QCoreApplication(sys.argv)
-    api = BinanceApi(API_KEY, API_SECRET)
-    is_processing = False
+class TriangularArbitrage(QObject):
 
-    def on_arbitrage_processed():
-        global is_processing
-        is_processing = False
+    def __init__(self):
+        super(TriangularArbitrage, self).__init__()
+        self.__api = BinanceApi(API_KEY, API_SECRET)
+        self.__is_processing = False
+        symbols_info = self.__api.get_symbols_info()
+        detector = ArbitrageDetector(
+            api=self.__api,
+            symbols_info=symbols_info,
+            fee=TRADE_FEE,
+            min_profit=MIN_PROFIT
+        )
+        detector.arbitrage_detected.connect(self.__process_arbitrage)
 
-    def process_arbitrage(arb):
-        global is_processing
-        if is_processing:
+    def __on_arbitrage_processed(self):
+        logger.info('Arbitrage processed')
+        self.__is_processing = False
+
+    def __process_arbitrage(self, arb):
+        if self.__is_processing:
             return
         else:
-            is_processing = True
+            self.__is_processing = True
         actions = []
         for action in arb.actions:
             actions.append(
@@ -36,21 +44,17 @@ if __name__ == '__main__':
                     timeInForce='GTC'
                 )
             )
-        executor = BinanceActionsExecutor(
-            api=api,
+        self.__executor = BinanceActionsExecutor(
+            api=self.__api,
             actions_list=actions
         )
-        executor.execution_finished.connect(on_arbitrage_processed)
-        executor.start()
+        self.__executor.execution_finished.connect(self.__on_arbitrage_processed)
+        self.__executor.start()
         logger.info('Arbitrage executor called')
 
-    symbols_info = api.get_symbols_info()
-    logger.debug('All Symbols Info: {}', symbols_info)
-    detector = ArbitrageDetector(
-        api=api,
-        symbols_info=symbols_info,
-        fee=TRADE_FEE,
-        min_profit=MIN_PROFIT
-    )
-    detector.arbitrage_detected.connect(process_arbitrage)
+
+if __name__ == '__main__':
+    logger.info('Starting...')
+    app = QCoreApplication(sys.argv)
+
     sys.exit(app.exec_())
