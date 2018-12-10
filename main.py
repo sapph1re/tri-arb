2d7d6cefba0bbe5f10239100a27e292ddefcf277
Roman V <dev.romanv@gmail.com>
2018-12-11 02:49:08 +0700

    Instantiating TriangularArbitrage (forgot it, lol). Moved created stuff to object members to prevent from destroying by garbage collector. Plus minor rearrangements.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from config import API_KEY, API_SECRET, TRADE_FEE, MIN_PROFIT
 from binance_api import BinanceApi
-from arbitrage_detector import ArbitrageDetector
+from arbitrage_detector import ArbitrageDetector, Arbitrage
 from binance_actions_executor import BinanceActionsExecutor, BinanceSingleAction
 from PyQt5.QtCore import QCoreApplication, QObject
 from custom_logging import get_logger
@@ -10,24 +10,24 @@ logger = get_logger(__name__)
 
 class TriangularArbitrage(QObject):
 
-    def __init__(self):
-        super(TriangularArbitrage, self).__init__()
-        self.__api = BinanceApi(API_KEY, API_SECRET)
+    def __init__(self, api: BinanceApi, parent=None):
+        super(TriangularArbitrage, self).__init__(parent)
+        self.__api = api
         self.__is_processing = False
-        symbols_info = self.__api.get_symbols_info()
-        detector = ArbitrageDetector(
+        self.__symbols_info = self.__api.get_symbols_info()
+        self.__detector = ArbitrageDetector(
             api=self.__api,
-            symbols_info=symbols_info,
+            symbols_info=self.__symbols_info,
             fee=TRADE_FEE,
             min_profit=MIN_PROFIT
         )
-        detector.arbitrage_detected.connect(self.__process_arbitrage)
+        self.__detector.arbitrage_detected.connect(self.__process_arbitrage)
 
     def __on_arbitrage_processed(self):
         logger.info('Arbitrage processed')
         self.__is_processing = False
 
-    def __process_arbitrage(self, arb):
+    def __process_arbitrage(self, arb: Arbitrage):
         if self.__is_processing:
             return
         else:
@@ -56,5 +56,6 @@ class TriangularArbitrage(QObject):
 if __name__ == '__main__':
     logger.info('Starting...')
     app = QCoreApplication(sys.argv)
-
+    api = BinanceApi(API_KEY, API_SECRET)
+    triarb = TriangularArbitrage(api)
     sys.exit(app.exec_())
